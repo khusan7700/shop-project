0062410bad813420bc07d7932beefc6b2450fdc1
Khusan <khusan7700@gmail.com>
2025-03-08 00:52:55 +0900

    feat: connect MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("server ----> OK");
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("🌟🌟🌟---MongoDB connection succeed---🌟🌟🌟");
+    const PORT = process.env.PORT ?? 3000;
+  })
+  .catch((err) => console.log("Error on connection MongoDB", err));
