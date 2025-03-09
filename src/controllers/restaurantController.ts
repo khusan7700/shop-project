2238f607dd20ad31ae935ef77ec8d722247fbad6
Khusan <khusan7700@gmail.com>
2025-03-09 20:21:37 +0900

    feat: develop authication (token and session) logic

--- src/controllers/restaurantController.ts
+++ src/controllers/restaurantController.ts
@@ -1,6 +1,6 @@
 import { T } from "../libs/types/common";
 import { Request, Response } from "express";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput, AdminRequest } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 import MemberService from "../models/Member.service";
 
@@ -31,24 +31,36 @@ restaurantController.getLogin = (req: Request, res: Response) => {
   }
 };
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
+restaurantController.processLogin = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Error, getLogin", err);
     res.send(err);
   }
 };
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.RESTAURANT;
     const result = await memberService.processSignup(newMember);
 
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Error, proessSignup", err);
     res.send(err);
