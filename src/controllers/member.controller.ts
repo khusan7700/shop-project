4c89273c18da52ce841076123e2715c351ab6d90
Khusan <khusan7700@gmail.com>
2025-04-03 03:30:04 +0900

    feat: develop updateMember business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,12 @@
 import MemberService from "../models/Member.service";
 import { T } from "../libs/types/common";
 import { NextFunction, Request, Response } from "express";
-import { ExtendedRequest, Member, MemberInput } from "../libs/types/member";
+import {
+  ExtendedRequest,
+  Member,
+  MemberInput,
+  MemberUpdateInput,
+} from "../libs/types/member";
 import { LoginInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
@@ -75,6 +80,22 @@ memberController.getMemberDetail = async (
   }
 };
 
+//----------------------------------updateMember-----------------------------------------------
+
+memberController.updateMember = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("update");
+    const input: MemberUpdateInput = req.body;
+    if (req.file) input.memberImage = req.file.path.replace(/\\/g, "/");
+    const result = await memberService.updateMember(req.member, input);
+
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, update", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
 //------------------------------------verifyAuth---------------------------------------------
 
 memberController.verifyAuth = async (
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -84,6 +84,21 @@ class MemberService {
     return result as unknown as Member;
   }
 
+  //------------------------------updateMember---------------------------------------//
+
+  public async updateMember(
+    member: Member,
+    input: MemberUpdateInput
+  ): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const result = await this.memberModel
+      .findOneAndUpdate({ _id: memberId }, input, { new: true })
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+    return result as unknown as Member;
+  }
+
   //------------------------------SSR---------------------------------------//
   //------------------------------processSignup---------------------------------------//
 
